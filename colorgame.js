6adcd4ee5f699e4fb5c74dcbbc3b0f4aa4657787
Billy <bdoddx13@outlook.com>
2019-05-20 22:56:44 -0400

    cleaned up the code. Still has issues

--- colorgame.js
+++ colorgame.js
@@ -11,30 +11,39 @@ function generateRGBValues (){
 	return x;
 }
 
+function hoverAnimations(){
+		//For Hover Animations on the squares and for the clicking of squares
+		var squareAnimations = document.querySelectorAll(".colored-area");
 
-function easyMode(){
-	//This is code to reset back to easy mode.(3 squares)
+		for (var x = 0; x < squareAnimations.length; x++){
+			squareAnimations[x].addEventListener("mouseover", function(){
+				this.classList.toggle("colored-area-hover");
+			});
+			squareAnimations[x].addEventListener("mouseleave", function(){
+				this.classList.toggle("colored-area-hover");
+			});
+		}
+}
 
-		var mode = "easy";
-		//debug
-		console.log("The mode is set to " + mode);
-		//Make all of this into a for loop 
-		//  I am reusing var's for no reason and it's wasteful
+
+function tileGenerater(x){
+	//Takes in the difficulty setting and applies random rgb colors to the 
+	//		tile using the generateRGBValues function
+	//		let x == onEasy
+
+	if (x == true){
 
 		// For Square 1 colorset
-		var rgbValues1 = generateRGBValues();
 		var RGBparse = rgbValues1[0] + "," + rgbValues1[1] + "," + rgbValues1[2];
-		var square = document.getElementById("01");
+		square = document.getElementById("01");
 		square.style.backgroundColor = "rgb(" + RGBparse + ")";
 
 		//For Square 2 colorset
-		var rgbValues2 = generateRGBValues();
 		var RGBparse = rgbValues2[0] + "," + rgbValues2[1] + "," + rgbValues2[2];
 		square = document.getElementById("02");
 		square.style.backgroundColor = "rgb(" + RGBparse + ")";
 
 		//For Square 3 colorset
-		var rgbValues3 = generateRGBValues();
 		var RGBparse = rgbValues3[0] + "," + rgbValues3[1] + "," + rgbValues3[2];
 		square = document.getElementById("03");
 		square.style.backgroundColor = "rgb(" + RGBparse + ")";
@@ -48,30 +57,61 @@ function easyMode(){
 
 		square = document.getElementById("06");
 		square.style.backgroundColor = "#FAFAFA";
+	}
 
-		//----------------------
-		//For loop to replacethe above code
-		// var rgbValues1 = generateRGBValues();
-		// var rgbValues2 = generateRGBValues();
-		// var rgbValues3 = generateRGBValues();
-		// var RGBArray = [rgbValues1,rgbValues2,rgbValues3];
-		// for (var i = 0; i < RGBArray.length; i++){
-		// 	var RGBparse = RGBArray[this][0] + "," + RGBArray[this][1] + "," + RGBArray[this][2];
-		// 	var square1 = document.getElementById
-		// }
+	else if(x == false){
 
-		//-----------------------
-		//For Hover Animations on the squares and for the clicking of squares
-		var squareAnimations = document.querySelectorAll(".colored-area");
+		// For Square 1 colorset
+		var RGBparse = rgbValues1[0] + "," + rgbValues1[1] + "," + rgbValues1[2];
+		var square = document.getElementById("01");
+		square.style.backgroundColor = "rgb(" + RGBparse + ")";
 
-		for (var x = 0; x < squareAnimations.length; x++){
-			squareAnimations[x].addEventListener("mouseover", function(){
-				this.classList.toggle("colored-area-hover");
-			});
-			squareAnimations[x].addEventListener("mouseleave", function(){
-				this.classList.toggle("colored-area-hover");
-			});
-		}
+		//For Square 2 colorset
+		var RGBparse = rgbValues2[0] + "," + rgbValues2[1] + "," + rgbValues2[2];
+		square = document.getElementById("02");
+		square.style.backgroundColor = "rgb(" + RGBparse + ")";
+
+		//For Square 3 colorset
+		var RGBparse = rgbValues3[0] + "," + rgbValues3[1] + "," + rgbValues3[2];
+		square = document.getElementById("03");
+		square.style.backgroundColor = "rgb(" + RGBparse + ")";
+
+		// For Square 4 colorset
+		var RGBparse = rgbValues4[0] + "," + rgbValues4[1] + "," + rgbValues4[2];
+		square = document.getElementById("04");
+		square.style.backgroundColor = "rgb(" + RGBparse + ")";
+
+		//For Square 5 colorset
+		var RGBparse = rgbValues5[0] + "," + rgbValues5[1] + "," + rgbValues5[2];
+		square = document.getElementById("05");
+		square.style.backgroundColor = "rgb(" + RGBparse + ")";
+
+		//For Square 6 colorset
+		var RGBparse = rgbValues6[0] + "," + rgbValues6[1] + "," + rgbValues6[2];
+		square = document.getElementById("06");
+		square.style.backgroundColor = "rgb(" + RGBparse + ")";
+
+	//----------------------
+	//For loop to replacethe above code
+	// var rgbValues1 = generateRGBValues();
+	// var rgbValues2 = generateRGBValues();
+	// var rgbValues3 = generateRGBValues();
+	// var RGBArray = [rgbValues1,rgbValues2,rgbValues3];
+	// for (var i = 0; i < RGBArray.length; i++){
+	// 	var RGBparse = RGBArray[this][0] + "," + RGBArray[this][1] + "," + RGBArray[this][2];
+	// 	var square1 = document.getElementById
+	// }
+
+	}
+}
+
+function easyMode(){
+	//This is code to reset back to easy mode.(3 squares)
+
+		var numOfSquares = document.querySelectorAll(".colored-area");
+
+		hoverAnimations();
+		tileGenerater(onEasy);
 
 		//-----------------------
 		//For detrmining the winning square
@@ -84,8 +124,8 @@ function easyMode(){
 		var clue = document.getElementById("winner").innerHTML = "RGB(" + RGBArray[winner] + ")";
 
 
-		for (var  x = 0; x < squareAnimations.length; x++){
-			squareAnimations[x].addEventListener("click", function(){
+		for (var  x = 0; x < numOfSquares.length; x++){
+			numOfSquares[x].addEventListener("click", function(){
 				var PlayerClicks = this.style.backgroundColor;
 				//debug lines to show the rgb value of what the player clicked vs the winner
 				console.log(PlayerClicks);
@@ -112,75 +152,9 @@ function easyMode(){
 
 
 function hardMode(){
-	// This is code for the optional hard mode. (6 squares)
-	var mode = "hard";
-	//debug
-	console.log("The mode is set to " + mode);
-	//------------------------
-	//Make all of this into a for loop 
-	//  I am reusing var's for no reason and it's wasteful
 
-	// For Square 1 colorset
-	var rgbValues1 = generateRGBValues();
-	var RGBparse = rgbValues1[0] + "," + rgbValues1[1] + "," + rgbValues1[2];
-	var square = document.getElementById("01");
-	square.style.backgroundColor = "rgb(" + RGBparse + ")";
-
-	//For Square 2 colorset
-	var rgbValues2 = generateRGBValues();
-	var RGBparse = rgbValues2[0] + "," + rgbValues2[1] + "," + rgbValues2[2];
-	square = document.getElementById("02");
-	square.style.backgroundColor = "rgb(" + RGBparse + ")";
-
-	//For Square 3 colorset
-	var rgbValues3 = generateRGBValues();
-	var RGBparse = rgbValues3[0] + "," + rgbValues3[1] + "," + rgbValues3[2];
-	square = document.getElementById("03");
-	square.style.backgroundColor = "rgb(" + RGBparse + ")";
-
-	// For Square 4 colorset
-	var rgbValues4 = generateRGBValues();
-	var RGBparse = rgbValues4[0] + "," + rgbValues4[1] + "," + rgbValues4[2];
-	square = document.getElementById("04");
-	square.style.backgroundColor = "rgb(" + RGBparse + ")";
-
-	//For Square 5 colorset
-	var rgbValues5 = generateRGBValues();
-	var RGBparse = rgbValues5[0] + "," + rgbValues5[1] + "," + rgbValues5[2];
-	square = document.getElementById("05");
-	square.style.backgroundColor = "rgb(" + RGBparse + ")";
-
-	//For Square 6 colorset
-	var rgbValues6 = generateRGBValues();
-	var RGBparse = rgbValues6[0] + "," + rgbValues6[1] + "," + rgbValues6[2];
-	square = document.getElementById("06");
-	square.style.backgroundColor = "rgb(" + RGBparse + ")";
-
-
-	//----------------------
-	//For loop to replacethe above code
-	// var rgbValues1 = generateRGBValues();
-	// var rgbValues2 = generateRGBValues();
-	// var rgbValues3 = generateRGBValues();
-	// var RGBArray = [rgbValues1,rgbValues2,rgbValues3];
-	// for (var i = 0; i < RGBArray.length; i++){
-	// 	var RGBparse = RGBArray[this][0] + "," + RGBArray[this][1] + "," + RGBArray[this][2];
-	// 	var square1 = document.getElementById
-	// }
-
-	//-----------------------
-	//For Hover Animations on the squares and for the clicking of squares
-	var squareAnimations = document.querySelectorAll(".colored-area");
-
-	for (var x = 0; x < squareAnimations.length; x++){
-		squareAnimations[x].addEventListener("mouseover", function(){
-			this.classList.toggle("colored-area-hover");
-		});
-		squareAnimations[x].addEventListener("mouseleave", function(){
-			this.classList.toggle("colored-area-hover");
-		});
-	}
-
+	var numOfSquares = document.querySelectorAll(".colored-area");
+	tileGenerater(onEasy);
 	//-----------------------
 	//For detrmining the winning square
 	//Determines a random index form the array "RGBArray"
@@ -192,8 +166,8 @@ function hardMode(){
 	var clue = document.getElementById("winner").innerHTML = "RGB(" + RGBArray[winner] + ")";
 
 
-	for (var  x = 0; x < squareAnimations.length; x++){
-		squareAnimations[x].addEventListener("click", function(){
+	for (var  x = 0; x < numOfSquares.length; x++){
+		numOfSquares[x].addEventListener("click", function(){
 			var PlayerClicks = this.style.backgroundColor;
 			//debug lines to show the rgb value of what the player clicked vs the winner
 			console.log(PlayerClicks);
@@ -215,14 +189,23 @@ function hardMode(){
 
 }
 
+//Sets random values for each color
+var rgbValues1 = generateRGBValues();
+var rgbValues2 = generateRGBValues();
+var rgbValues3 = generateRGBValues();
+var rgbValues4 = generateRGBValues();
+var rgbValues5 = generateRGBValues();
+var rgbValues6 = generateRGBValues();
+
 //radio buttons for difficulty and new game
 var easyButton = document.getElementById("easy difficulty");
 var hardButton = document.getElementById("hard difficulty");
 var resetGame = document.getElementById("reset");
 
 //Starts the game off in easy mode
-easyMode();
 var onEasy = true;
+easyMode();
+
 
 //Switches the game back to easy mode
 easyButton.addEventListener("click", function(){
