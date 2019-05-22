feb0dc7696af7b7bb885e0536c830846d49ecf4e
Billy <bdoddx13@outlook.com>
2019-05-21 22:46:16 -0400

    Cleaned up js code

--- colorgame.js
+++ colorgame.js
@@ -11,44 +11,47 @@ function generateRGBValues (){
 	return x;
 }
 
-function hoverAnimations(){
-		//For Hover Animations on the squares and for the clicking of squares
-		var squareAnimations = document.querySelectorAll(".colored-area");
-
-		for (var x = 0; x < squareAnimations.length; x++){
-			squareAnimations[x].addEventListener("mouseover", function(){
-				this.classList.toggle("colored-area-hover");
-			});
-			squareAnimations[x].addEventListener("mouseleave", function(){
-				this.classList.toggle("colored-area-hover");
-			});
+function gameMode(){
+	//Sets random values for each color
+	var rgbValues1 = generateRGBValues();
+	var rgbValues2 = generateRGBValues();
+	var rgbValues3 = generateRGBValues();
+	if (onEasy == false){
+		var rgbValues4 = generateRGBValues();
+		var rgbValues5 = generateRGBValues();
+		var rgbValues6 = generateRGBValues();
 		}
-}
 
+	//For Hover Animations on the squares and for the clicking of squares
+	var squareAnimations = document.querySelectorAll(".colored-area");
 
-function tileGenerater(x){
-	//Takes in the difficulty setting and applies random rgb colors to the 
-	//		tile using the generateRGBValues function
-	//		let x == onEasy
+	for (var x = 0; x < squareAnimations.length; x++){
+		squareAnimations[x].addEventListener("mouseover", function(){
+			this.classList.toggle("colored-area-hover");
+		});
+		squareAnimations[x].addEventListener("mouseleave", function(){
+			this.classList.toggle("colored-area-hover");
+		});
+	}
 
-	if (x == true){
+		if (onEasy == true){
 
 		// For Square 1 colorset
 		var RGBparse = rgbValues1[0] + "," + rgbValues1[1] + "," + rgbValues1[2];
 		square = document.getElementById("01");
 		square.style.backgroundColor = "rgb(" + RGBparse + ")";
 
-		//For Square 2 colorset
+		//For Square 2 colorset	
 		var RGBparse = rgbValues2[0] + "," + rgbValues2[1] + "," + rgbValues2[2];
 		square = document.getElementById("02");
 		square.style.backgroundColor = "rgb(" + RGBparse + ")";
 
-		//For Square 3 colorset
+		//For Square 3 colorset	
 		var RGBparse = rgbValues3[0] + "," + rgbValues3[1] + "," + rgbValues3[2];
 		square = document.getElementById("03");
 		square.style.backgroundColor = "rgb(" + RGBparse + ")";
 
-		// For setting the 4-6 squares back to neutral color in the event of a difficulty switch
+		// For setting the 4-6 squares back to neutral color in the event of a difficulty switch		
 		square = document.getElementById("04");
 		square.style.backgroundColor = "#FAFAFA";
 
@@ -59,7 +62,7 @@ function tileGenerater(x){
 		square.style.backgroundColor = "#FAFAFA";
 	}
 
-	else if(x == false){
+	else if(onEasy == false){
 
 		// For Square 1 colorset
 		var RGBparse = rgbValues1[0] + "," + rgbValues1[1] + "," + rgbValues1[2];
@@ -103,63 +106,19 @@ function tileGenerater(x){
 	// }
 
 	}
-}
 
-function easyMode(){
-	//This is code to reset back to easy mode.(3 squares)
-
-		var numOfSquares = document.querySelectorAll(".colored-area");
-
-		hoverAnimations();
-		tileGenerater(onEasy);
-
-		//-----------------------
-		//For detrmining the winning square
-		//Determines a random index form the array "RGBArray"
-		var RGBArray = [rgbValues1,rgbValues2,rgbValues3];
-		var winner = Math.floor(Math.random() * Math.floor(3));
-
-
-		//Set the H1 element in the title banner to match the winner 
-		var clue = document.getElementById("winner").innerHTML = "RGB(" + RGBArray[winner] + ")";
-
-
-		for (var  x = 0; x < numOfSquares.length; x++){
-			numOfSquares[x].addEventListener("click", function(){
-				var PlayerClicks = this.style.backgroundColor;
-				//debug lines to show the rgb value of what the player clicked vs the winner
-				console.log(PlayerClicks);
-				console.log(RGBArray[winner]);
-		
-			if (PlayerClicks == "rgb(" + RGBArray[winner][0] + ", " + RGBArray[winner][1] + ", " + RGBArray[winner][2] + ")"){
-				console.log("We have a winner");
-				document.querySelector(".banner").style.backgroundColor = "rgb(" + RGBArray[winner][0] + ", " + RGBArray[winner][1] + ", " + RGBArray[winner][2] + ")";
-				document.querySelector("#Annoucement").innerHTML = "We have a Winner!";
-				}
-			else{
-				console.log("Nope, try again");
-				document.querySelector("#Annoucement").innerHTML = "Nope, try again...";
-			}
-
-		});
-		
-		};
-
-}
-
-
-
-
-
-function hardMode(){
-
-	var numOfSquares = document.querySelectorAll(".colored-area");
-	tileGenerater(onEasy);
 	//-----------------------
 	//For detrmining the winning square
 	//Determines a random index form the array "RGBArray"
-	var RGBArray = [rgbValues1,rgbValues2,rgbValues3,rgbValues4,rgbValues5,rgbValues6];
-	var winner = Math.floor(Math.random() * Math.floor(6));
+	if(onEasy == true){
+		var RGBArray = [rgbValues1,rgbValues2,rgbValues3];
+		var winner = Math.floor(Math.random() * Math.floor(3));
+	}
+	else{
+		var RGBArray = [rgbValues1,rgbValues2,rgbValues3,rgbValues4,rgbValues5,rgbValues6];
+		var winner = Math.floor(Math.random() * Math.floor(6));
+	}
+
 
 
 	//Set the H1 element in the title banner to match the winner 
@@ -169,10 +128,11 @@ function hardMode(){
 	for (var  x = 0; x < numOfSquares.length; x++){
 		numOfSquares[x].addEventListener("click", function(){
 			var PlayerClicks = this.style.backgroundColor;
+			
 			//debug lines to show the rgb value of what the player clicked vs the winner
 			console.log(PlayerClicks);
 			console.log(RGBArray[winner]);
-	
+
 		if (PlayerClicks == "rgb(" + RGBArray[winner][0] + ", " + RGBArray[winner][1] + ", " + RGBArray[winner][2] + ")"){
 			console.log("We have a winner");
 			document.querySelector(".banner").style.backgroundColor = "rgb(" + RGBArray[winner][0] + ", " + RGBArray[winner][1] + ", " + RGBArray[winner][2] + ")";
@@ -183,19 +143,10 @@ function hardMode(){
 			document.querySelector("#Annoucement").innerHTML = "Nope, try again...";
 		}
 
-		});
+	});
+
 	};
-
-
 }
-
-//Sets random values for each color
-var rgbValues1 = generateRGBValues();
-var rgbValues2 = generateRGBValues();
-var rgbValues3 = generateRGBValues();
-var rgbValues4 = generateRGBValues();
-var rgbValues5 = generateRGBValues();
-var rgbValues6 = generateRGBValues();
 
 //radio buttons for difficulty and new game
 var easyButton = document.getElementById("easy difficulty");
@@ -204,9 +155,14 @@ var resetGame = document.getElementById("reset");
 
 //Starts the game off in easy mode
 var onEasy = true;
-easyMode();
+var numOfSquares = document.querySelectorAll(".colored-area");
 
 
+gameMode();
+
+
+
+//Option Buttons---------------------------------------------------------------------
 //Switches the game back to easy mode
 easyButton.addEventListener("click", function(){
 	if(onEasy == false){
@@ -214,7 +170,7 @@ easyButton.addEventListener("click", function(){
 		document.querySelector("#Annoucement").innerHTML = "Pick The Color That Matches";
 		//sets global difficulty
 		onEasy = true;
-		easyMode();
+		gameMode();
 	}
 
 });
@@ -226,7 +182,7 @@ hardButton.addEventListener("click", function(){
 		document.querySelector("#Annoucement").innerHTML = "Pick The Color That Matches";
 		//sets global difficulty
 		onEasy = false;
-		hardMode();
+		gameMode();
 	}
 
 });
@@ -239,7 +195,7 @@ resetGame.addEventListener("click", function(){
 		document.querySelector("#Annoucement").innerHTML = "Pick The Color That Matches";
 		//sets global difficulty
 		onEasy = true;
-		easyMode();
+		gameMode();
 	}
 
 	else if(onEasy == false){
@@ -247,6 +203,6 @@ resetGame.addEventListener("click", function(){
 		document.querySelector("#Annoucement").innerHTML = "Pick The Color That Matches";
 		//sets global difficulty
 		onEasy = false;
-		hardMode();
+		gameMode();
 	}
 });
